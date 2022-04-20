cb684ef8db17a274252aa66b44bd2919609a3bb7
Deyao Chen <chendeyao000@gmail.com>
2022-04-21 00:35:34 +0800

    init

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Introduction
+The machines in the labs can be quite difficult to work with sometimes especially with broken gcc complier, no package manager, and chsh not working. This script tries to side step the problem with homebrew, which can install packages in user space. It also comes with some (opinionated) niceties like oh-my-zsh, syntax highlighting and auto completion, which are quite useful. 
+
+# Installation
+```bash
+/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/setup.sh)"
+```
+
+
+# Troubleshooting
+1. Build failure with `brew install`.
+
+    The make complier on the lab machines is a bit broken. We haven't found a way to fix it yet, but you can workaround many build failures with `brew install --force-bottle` to use a "bottle" (pre-complied binary). Homebrew defaults to building from source because it is not installed in `/home/linuxbrew/.linuxbrew` (for which you don't have write permission). 
+
+2. Problem with PATH.
+
+    Try to add the PATH in `~/.profile`, which will get loaded.
+
+# Customization
+The theme used is powerlevel10k, applied on oh-my-zsh. the font for p10k is chosen for the compatibility. If you want to customize the prompt look, run `p10k configure`. 
+
+You can also read more about the two packages to learn more about what they can do. 
---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+cd ${HOME}
+git clone https://github.com/Homebrew/brew ${HOME}/.linuxbrew
+.linuxbrew/bin/brew update --force --quiet
+echo 'eval "$(.linuxbrew/bin/brew shellenv)"' >> ${HOME}/.profile
+eval "$(.linuxbrew/bin/brew shellenv)"
+brew install --force-bottle binutils
+brew install --force-bottle gcc
+brew install zsh
+
+cd ${HOMEBREW_PREFIX}/bin
+ln -s gcc-11 gcc 
+ln -s g++-11 g++ 
+ln -s cpp-11 cpp 
+ln -s c++-11 c++
+
+cd ${HOME}
+NO_SHELL=false
+if [ -n "$BASH_VERSION" ]; then
+  echo "(which zsh)" >> ${HOME}/.bashrc
+elif [ -n "$ZSH_VERSION" ]; then
+  :
+else
+  NO_SHELL=true
+fi
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ${HOME}/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+if [ -f "${HOME}/.zshrc" ]; then
+  ZSHRC_BACKUP="${HOME}/.zshrc.backup"
+  while [ -f "${ZSHRC_BACKUP}" ]; do
+    ZSHRC_BACKUP="${ZSHRC_BACKUP}.backup"
+  done
+    ZSHRC_BACKUP_REL=$(realpath --relative-to="${HOME}" "${ZSHRC_BACKUP}")
+    echo "~/.zshrc exists, moving it to ~/${ZSHRC_BACKUP_REL}"
+    mv ${HOME}/.zshrc ${ZSHRC_BACKUP}
+fi
+curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.zshrc > .zshrc
+
+if [ -f "${HOME}/.p10k.zsh" ]; then
+  P10K_BACKUP="${HOME}/.p10k.zsh.backup"
+  while [ -f "${P10K_BACKUP}" ]; do
+    P10K_BACKUP="${P10K_BACKUP}.backup"
+  done
+    P10K_BACKUP_REL=$(realpath --relative-to="${HOME}" "${P10K_BACKUP}")
+    echo "~/.p10k.zsh exists, moving it to ~/${P10K_BACKUP_REL}"
+    mv ${HOME}/.p10k.zsh ${P10K_BACKUP}
+fi
+curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.p10k.zsh > .p10k.zsh
+
+if [ "$NO_SHELL" = true ]; then
+  echo "Install successful, you don't seem to be using either bash or zsh so I can't figure out how to change your shell to zsh. Using zsh is not necessary, but it has some nice things installed like syntax highlighting and auto completion"
+fi
