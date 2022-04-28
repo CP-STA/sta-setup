89662ffa81e4deb83f1e883ff1520f61464c6091
Deyao Chen <chendeyao000@gmail.com>
2022-04-28 12:30:13 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,23 +36,43 @@ if [ -f "${HOME}/.zshrc" ]; then
   while [ -f "${ZSHRC_BACKUP}" ]; do
     ZSHRC_BACKUP="${ZSHRC_BACKUP}.backup"
   done
-    ZSHRC_BACKUP_REL=$(realpath --relative-to="${HOME}" "${ZSHRC_BACKUP}")
-    echo "~/.zshrc exists, moving it to ~/${ZSHRC_BACKUP_REL}"
-    mv ${HOME}/.zshrc ${ZSHRC_BACKUP}
+  ZSHRC_BACKUP_REL=$(realpath --relative-to="${HOME}" "${ZSHRC_BACKUP}")
+  echo "~/.zshrc exists, moving it to ~/${ZSHRC_BACKUP_REL}"
+  mv ${HOME}/.zshrc ${ZSHRC_BACKUP}
 fi
-curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.zshrc > ${HOME}/.zshrc
+
+NO_ZSHRC = false
+if [ -f "${HOME}/.zshrc" ]; then
+  NO_ZSHRC = true
+else
+  curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.zshrc > ${HOME}/.zshrc
+fi
 
 if [ -f "${HOME}/.p10k.zsh" ]; then
   P10K_BACKUP="${HOME}/.p10k.zsh.backup"
   while [ -f "${P10K_BACKUP}" ]; do
     P10K_BACKUP="${P10K_BACKUP}.backup"
   done
-    P10K_BACKUP_REL=$(realpath --relative-to="${HOME}" "${P10K_BACKUP}")
-    echo "~/.p10k.zsh exists, moving it to ~/${P10K_BACKUP_REL}"
-    mv ${HOME}/.p10k.zsh ${P10K_BACKUP}
+  P10K_BACKUP_REL=$(realpath --relative-to="${HOME}" "${P10K_BACKUP}")
+  echo "~/.p10k.zsh exists, moving it to ~/${P10K_BACKUP_REL}"
+  mv ${HOME}/.p10k.zsh ${P10K_BACKUP}
 fi
-curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
+
+NO_P10K = false
+if [ -f "${HOME}/.p10k.zsh" ]; then
+  NO_P10K = true 
+else
+  curl -fsSL https://raw.githubusercontent.com/STAOJ/sta-setup/master/.p10k.zsh > ${HOME}/.p10k.zsh
+fi
 
 if [ "$NO_SHELL" = true ]; then
-  echo "Install successful, you don't seem to be using either bash or zsh so I can't figure out how to change your shell to zsh. Using zsh is not necessary, but it has some nice things installed like syntax highlighting and auto completion"
+  echo "Install successful, you don't seem to be using either bash or zsh so we can't figure out how to change your shell to zsh. Using zsh is not necessary, but it has some nice things installed like syntax highlighting and auto completion"
+fi
+
+if [ "$NO_ZSHRC" = true ]; then
+  echo "~/.zshrc already exists and we are unable to create an backup of it. So the recommended ~/.zshrc is not install. To install it, consider removing your ~/.zshrc and run the script again."
+fi
+
+if [ "NO_P10K" = true ]; then 
+  echo "~/.p10k.zsh already exists and we are unable to create an backup of it. So the recommended ~/.p10k.zsh is not install. To install it, consider removing your ~/.p10k.zsh and run the script again."
 fi
